cff930e877277910ad5c72a01e7863e21ed818ca
Qcohen22 <cohenjr.quincy@live.jobcorps.org>
2022-03-31 19:05:12 +0000

    todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
+	const [item, setItem] = useState("");
+	const [todolist, setTodolist] = useState([]);
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div>
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Username"
+					onCharge={(e) => setItem(e.target.value)}
+					value={item}
+				/>
+				<a
+					onClick={() => {
+						setTodolist([...todolist, item]);
+						console.log(todolist);
+					}}
+					type="button"
+					className="btn btn-primary"
+					id="basic-addon1">
+					Button
+				</a>
+			</div>
+			<ul>
+				<li>{item}</li>
+			</ul>
+		</>
 	);
 };
 
